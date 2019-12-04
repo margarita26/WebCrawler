d727415d07da04583af99b84d4cd74b18e6df9a1
Margarita <gubanova.marg@yandex.com>
2019-12-04 01:30:28 -0500

    save current work

--- people_finder.py
+++ people_finder.py
@@ -7,6 +7,7 @@ from dateutil.relativedelta import relativedelta
 from datetime import datetime
 import random
 
+#~~~~~~~~~~~~~~~~~~~~~~~~ Part 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #list of user agent to bypass captcha going to be chosen randomly
 user_agent_list = [
    #Chrome
@@ -38,14 +39,13 @@ user_agent_list = [
 
 Person = namedtuple('Person',('fist', 'midlle','last','dob', 'city', 'state'))
 
+#class with static methods that queries persons public record information from truepeoplesearch
 class PeopleFider():
-
+    # if you dont want to use one of the fields pass in an empty string,
+    # site is still going to work if at least one of the params are filled out
     @staticmethod
-    def query(first, last, middle, dob,city,state):
+    def query(first,last,middle,dob,city,state):
         global user_agent_list
-        #init a dictionary with no values
-        keys = ['current_adr','past_adr','phone_numbers','email','associated_names','relatives','associates','businesses']
-        requested_info = {key: None for key in keys}
         #get age
         birth_day = datetime.strptime(dob, '%Y-%m-%d')
         age = relativedelta(datetime.today(),birth_day).years
@@ -58,16 +58,19 @@ class PeopleFider():
         url = "https://www.truepeoplesearch.com/results?"
         #bypass captcha
         user_agent = random.choice(user_agent_list)
-        #create a link for search
+        #create a link for search with personal details
         url_result = url + urlencode(person_params)
         search_result = Request(url_result, headers={'User-Agent':user_agent})
         #open the link with header and read teh result
         page = urlopen(search_result)
+        #use beautiful soup for easier html parsing
         soup = BeautifulSoup(page.read(), 'lxml')
-        #check something came up in search
+        #finds links to all people matched with paramters if found
+        #the long class name appears when there is a button with 'View Details'
         anchors = soup.find_all('a', {'class': 'btn btn-success btn-lg detail-link shadow-form', 'href': True})
-        if len(anchors) > 0:
-            #person is found, view their details
+        l =  len(anchors)
+        if l > 0:
+            #person is found, view details of the first person in search list
             url_result += '&rid=0x0'
             #bypass captcha
             user_agent = random.choice(user_agent_list)
@@ -75,12 +78,15 @@ class PeopleFider():
             page2 = urlopen(search_result)
             soup = BeautifulSoup(page2.read(), 'lxml')
             print(soup.prettify())
+            #return get_info(soup)
+        return None
 
-        else:
-            #person is not found
-            return None
+    #parse and return dictionary of required info form beautiful soup
+    @staticmethod
+    def get_info(soup):
+        keys = ['current_adr','past_adr','phone_numbers','email','associated_names','relatives','associates','businesses']
+        requested_info = {key: None for key in keys}
 
         return requested_info
 
-
-PeopleFider.query('Lucy', 'Zang', '', '1991-10-26', 'Boston', 'MA')
+PeopleFider.query('Lu', 'Zang', '', '1964-10-26', 'San Diego', 'CA')
